c8e17207e892d2f094060c8678659d1465e1575f
Ram-7655 <padhy0731@gmail.com>
2025-03-23 19:06:19 +0900

    feature201 changes

--- Application.java.java
+++ Application.java.java
@@ -10,4 +10,20 @@ public class Animal {
     // Protected method
     protected void makeSound() {
         System.out.println("Some generic animal sound");
+#feature201 changes by vivek on october2024 for services module
+// File: Dog.java
+
+// Another class in the same package
+public class Dog {
+    Animal myAnimal = new Animal();
+
+    void displayInfo() {
+        // Accessing the default (package-private) field and method
+        System.out.println("Species: " + myAnimal.species);
+        myAnimal.makeSound();
+    }
+
+}
+#end of feature201 code
+
     }
