20a4ef3067bc2aa6ef7190b4fcb3e0cad34f68cd
Ram-7655 <padhy0731@gmail.com>
2025-03-23 19:20:33 +0900

    feature202 changes

--- Application.java.java
+++ Application.java.java
@@ -10,4 +10,21 @@ public class Animal {
     // Protected method
     protected void makeSound() {
         System.out.println("Some generic animal sound");
+#feature202 by ethan in october2024 on reports module
+public class Dog {
+    // Instance variables
+    String name;
+    int age;
+
+    // Constructor to initialize the instance variables
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Instance method to bark
+    public void bark() {
+        System.out.println(name + " says Woof!");
+    }
+#end of feature202 code
     }
